57d9422248f33d0d6bfde00dfa92bf77cc18471d
jamsea <jhush91@gmail.com>
2011-09-19 14:16:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Perpetuate::Application.config.secret_token = '7aa09533bce8dbf8aba757500452217c64b3cc50b81534f7cf9af7779cd2604e02fc18f1ac0efee3f0f8b14db6909f9c4f35e9764434e53e252bc5f2f77ccb40'
