c43c4b60b6f2f4e4c97ba1612fd376332a003130
Jamie Hush <jhush91@gmail.com>
2011-12-01 19:49:21 -0500

    Still changing Dreamhost config files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,32 @@
 require 'bundler/capistrano'
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
 
-#set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 default_run_options[:pty] = false
 ssh_options[:forward_agent] = true
 set :use_sudo, false
-set :user, "underdogdev"
+set :user, "perpetuate"
+set :domain, 'perpetuate.dreamhosters.com'
+set :project, 'perpetuate'
 
-set :application, "perpetuate"
-set :repository,  "git@github.com:claco/perpetuate.git"
+set :application, "perpetuate.dreamhosters.com"
+set :repository,  "git@github.com:jamsea/perpetuate.git"
 set :scm, :git
 set :branch, 'master'
 set :git_shallow_clone, 1
 set :deploy_via, :remote_cache
 set :copy_compression, :bz2
 set :rails_env, 'production'
-set :deploy_to, "/home/perpetuate/#{application}"
-
+set :deploy_to, "/home/perpetuate/perpetuate"
+set :deploy_via, :export
 
 
 
 role :web, "#{application}"                          # Your HTTP server, Apache/etc
 role :app, "#{application}"                           # This may be the same as your `Web` server
 role :db,  "#{application}", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+#role :db,  "your slave db-server here"
+
+set :chmod755, "app config db lib public vendor script script/* public/disp*"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
